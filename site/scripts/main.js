7a608f87fa6bc5f389dab9d1e071c2e1cecfcdc0
talrez11 <tal@way2cu.com>
2016-03-07 11:16:04 +0300

    Script: Updated function displaying product price and name according to checked element

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -1257,7 +1257,7 @@ Site.on_load = function() {
 	input_elements.on('click', function() {
 		var item = $(this);
 		var price = item.attr('value');
-		var name = item.attr('name');
+		var name = item.attr('id');
 		figure_size.html(name);
 		figure_price.html(price);
 	})
