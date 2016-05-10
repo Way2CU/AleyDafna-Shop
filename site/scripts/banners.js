3a27b8e018d8090db93b7398f4ece54304d08174
Mladen Mijatov <meaneye.rcf@gmail.com>
2016-05-10 16:21:06 +0300

    Redone some of the filtering code.

--- site/scripts/banners.js
+++ site/scripts/banners.js
@@ -6,12 +6,15 @@
  * Authors: Mladen Mijatov
  */
 
-var Site = Site || {};
+var Site = Site || new Object()
+
 
 Site.BannerSystem = function() {
 	var self = this;
 
-	self.handler = {};
+	self.show_interval = 8;
+	self.links = new Array();
+	self.handler = new Object()
 
 	/**
 	 * Object initialization function
@@ -19,7 +22,7 @@ Site.BannerSystem = function() {
 	self._init = function() {
 		// connect events
 		if (Site.filter)
-			Site.filter.connect(self.handler.item_visibility_change);
+			Site.filter.events.connect('visibility-change', self.handler.item_visibility_change);
 
 		// load links from the server
 		self._load_links();
@@ -29,37 +32,82 @@ Site.BannerSystem = function() {
 	 * Load links from the server.
 	 */
 	self._load_links = function() {
+		var data = {
+				'group': 'banners'
+			};
+
 		new Communicator('links')
 			.on_success(self.handler.links_load)
 			.on_error(self.handler.links_load_error)
-			.get('json_links_list');
+			.get('json_link_list', data);
 	};
 
 	/**
 	 * Place banners on page.
 	 */
 	self._position_banners = function() {
+		if (self.links.length == 0)
+			return;
+
+		// place links in DOM tree
+		for (var i=0, count=self.links.length; i<count; i++) {
+			var link = self.links[i];
+			var position = i * self.show_interval - i;
+
+
+		}
 	};
 
 	/**
 	 * Handle event sent by the filter object when item visibility changes.
 	 */
-	self.handler.item_visibility_change = function() {
-		self._position_banners();
+	self.handler.item_visibility_change = function(visible_items) {
+		self._position_banners(visible_items);
 	};
 
 	/**
 	 * Handle banner links load.
 	 */
 	self.handler.links_load = function(data) {
+		if (data.error)
+			return;
+
+		// create banners from link data
+		for (var index in data.items) {
+			var link_data = data.items[index];
+			var link = document.createElement('a');
+			var image = document.createElement('img');
+
+			// configure image
+			image.setAttribute('src', link_data.image);
+			image.setAttribute('alt', link_data.text[language_handler.current_language]);
+			image.setAttribute('width', 582);
+			image.setAttribute('height', 282);
+
+			// configure link
+			link.setAttribute('href', link_data.redirect_url);
+			link.appendChild(image);
+
+			// add link to the list
+			self.links.push(link);
+		}
+
+		// position links
+		self._position_banners();
 	};
 
 	/**
 	 * Handle communication or server error when loading banner links.
 	 */
 	self.handler.links_load_error = function() {
+		self.links = new Array();
 	};
-	
+
 	// finalize object
 	self._init();
 };
+
+
+$(function() {
+	Site.banner_system = new Site.BannerSystem();
+});
---
+++ site/scripts/filter.js
@@ -0,0 +1,155 @@
+/**
+ * Item Filter JavaScript
+ * Aley Dafna
+ *
+ * Copyright (c) 2016. by Way2CU, http://way2cu.com
+ * Authors: Mladen Mijatov, Tal Reznic
+ */
+var Site = Site || new Object();
+
+
+/**
+ * @param object parent_container_selector - CSS query selector for content section
+ * @param object categories_selector       - CSS query selector for individual categories
+ * @param object item                      - CSS query selector for item located inside of category
+ * @triggers visibility-change
+ */
+Site.QuickFilter = function(parent_container_selector, categories_selector, item_selector) {
+	var self = this;
+
+	self.parent_container = $(parent_container_selector);
+	self.categories = self.parent_container.find(categories_selector);
+	self.checkbox_container = null;
+	self.items_list = new Object();
+	self.events = null;
+	self.container = null;
+
+	/**
+	 * Complete object initialization.
+	 */
+	self._init = function() {
+		// create event handler
+		self.events = new Caracal.EventSystem();
+		self.events.register('visibility-change');
+
+		// create checkboxes container
+		self.checkbox_container = $('<div id="checkboxes">').appendTo(self.parent_container);
+
+		// create container for unique list items
+		self.container = $('<div id="unique">').appendTo(self.parent_container);
+
+		// create checkbox element for all categories
+		self.categories.each(function() {
+			var category = $(this);
+			var category_name = category.find('h5').text();
+			var category_id = category.attr('id');
+
+			if (category.find(item_selector).length > 0)
+				self._create_checkbox(category_name, category_id);
+
+			category.remove();
+		});
+
+		// create unique items list
+		self.categories.find(item_selector).each(function() {
+			var item = $(this);
+			var uid = item.data('uid');
+			var category_id = item.parent().attr('id');
+
+			if (!(uid in self.items_list)) {
+				// add item to unique list
+				self.items_list[uid] = item;
+				self.container.append(item);
+
+				// create categories list
+				var categories = new Array();
+				item.data('categories', categories);
+
+			} else {
+				// get categories list from existing item
+				var categories = self.items_list[uid].data('categories');
+			}
+
+			// append category to the list
+			categories.push(category_id);
+		});
+
+		// create default checkbox element
+		if (self.categories.length > 0)
+			self._create_checkbox(language_handler.getText(null, 'default_checkbox_title'));
+	}
+
+	/**
+	 * Create checkbox element.
+	 *
+	 * @param string name
+	 * @param string id
+	 */
+	self._create_checkbox = function(category_name, id) {
+		// create radio button
+		var input = $('<input>');
+		input
+			.attr('type', 'radio')
+			.attr('name', 'sort')
+			.attr('data-id', id)
+			.on('change', self._handle_category_toggle);
+
+		// create text container
+		var span = $('<span>');
+		span.text(category_name);
+
+		// create label object
+		var label = $('<label>');
+		label
+			.append(input)
+			.append(span)
+			.appendTo(self.checkbox_container);
+	}
+
+	/**
+	 * Show filtered items.
+	 */
+	self._handle_category_toggle = function(event) {
+		// prevent default radio button behavior
+		event.preventDefault();
+
+		var item = $(this).parent();
+		var items = new Array();
+		var category = $(this).data('id');
+
+		// highlight current radio button
+		self.checkbox_container.find('label').not(item).removeClass('active');
+		item.addClass('active');
+
+		// show all items
+		if (category == undefined) {
+			for (var uid in self.items_list) {
+				self.items_list[uid].removeClass('hidden');
+				items.push(item);
+			}
+
+			return;
+		}
+
+		// show and hide items accordingly
+		for (var uid in self.items_list) {
+			var item = self.items_list[uid];
+			var item_categories = item.data('categories');
+
+			// check item membership
+			if (item_categories.indexOf(category.toString()) >= 0) {
+				item.removeClass('hidden');
+				items.push(item);
+
+			} else {
+				item.addClass('hidden');
+			}
+		}
+
+		// trigger event
+		self.events.trigger('visibility-change', items);
+	}
+
+	// finalize object
+	self._init();
+}
